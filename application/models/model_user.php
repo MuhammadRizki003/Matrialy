a6986bd05011e5b8e5395ecd550a64d14943bd5f
Muhammad Rizki <105654161+MuhammadRizki003@users.noreply.github.com>
2023-10-19 08:48:22 +0700

    Update model_user.php
    add alamat

--- application/models/model_user.php
+++ application/models/model_user.php
@@ -14,4 +14,36 @@ class Model_user extends CI_Model{
 	public function tampiluser(){
 		return $this->db->get_where('tb_user',array('role_id' => 2));
 	}
+	public function tampilkota()
+	{
+		return $this->db->get("tb_ongkir");
+	}
+	public function cariDataAlamat($where)
+	{
+		$result = $this->db->get_where("tb_alamat", $where);
+		if ($result->num_rows() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public function tambahAlamat($data, $table)
+	{
+		$this->db->insert($table, $data);
+	}
+	public function ubahAlamat($where, $update, $table)
+	{
+		$this->db->where($where);
+		$this->db->update($table, $update);
+	}
+	public function tampilAlamat($id)
+	{
+		$data = array('id_user' => $id);
+		$result = $this->db->where($data)->get('tb_alamat');
+		if ($result->num_rows() > 0) {
+			return $result->row_array();
+		} else {
+			return false;
+		}
+	}
 }
