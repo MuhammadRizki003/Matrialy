1d122dac072ad8dae2070d009dbcc7f7db58afae
Muhammad Rizki <105654161+MuhammadRizki003@users.noreply.github.com>
2023-10-19 08:47:30 +0700

    Update userdata.php
    update setting alamat

--- application/views/auth/userdata.php
+++ application/views/auth/userdata.php
@@ -29,8 +29,30 @@
 					</table>
 				</div>
 			</div>
+			<label>Alamat</label>
+			<div class="card col-lg-4 col-md-5 col-12">
+				<div class="card-body">
+					<table class="table table-borderless">
+						<tr>
+							<td><?php if ($alamat == false) { ?>
+									Anda belum mengatur alamat anda
+								<?php
+								} else {
+									echo $alamat['alamat'];
+								}
+								?></td>
+							<td align="right"><a href="#edit_alamat" data-toggle='modal' class="card-link">
+									<?php if ($alamat == false) { ?>
+										Tambah&nbsp;Alamat
+										<?php
+									} else {
+										?>Edit&nbsp;Alamat <?php } ?></a></td>
+						</tr>
+					</table>
+				</div>
+			</div>
 			<a href="#edit_password" data-toggle='modal'>
-				<button type="button" class="btn btn-primary">Ganti Password</button>
+				<button type="button" class="btn btn-primary mb-5">Ganti Password</button>
 			</a>
 		</div>
 	</div>
@@ -118,6 +140,49 @@
 					<div class="form-group">
 						<label>Konfirmasi Password Baru</label>
 						<input type="password" name="password_baru2" class="form-control" placeholder="Konfirmasi Password Baru" autocomplete="off">
+					</div>
+					<div>
+						<?php echo anchor('auth/info_akun/', '<button type="button" class="btn btn-danger mr-1" >Batal</button>') ?>
+						<button type="submit" class="btn btn-primary">Perbarui data</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- baru -->
+<div class="modal fade" id="edit_alamat" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-edit mr-2"></i>Form Edit Akun</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form method="post" action="<?php echo base_url() . 'auth/alamat/' ?>">
+					<input type="hidden" name="id_user" value="<?php echo $this->session->userdata('id_user'); ?>">
+					<div class="form-group">
+						<label>Pilih Kota</label>
+						<select name=kota class="form-control form-control-md" required>
+							<option hidden value="<?php if ($alamat != false) {
+														echo $alamat['kota'];
+													}
+													?> ">Pilih kota</option>
+							<?php
+							foreach ($kota as $k) {
+							?>
+								<option style="font-size: large;" value="<?php echo $k->kota ?>"><?php echo $k->kota ?></option>
+							<?php } ?>
+						</select>
+					</div>
+					<div class="form-group">
+						<label>Masukan Alamat Lengkap</label>
+						<textarea name="alamat" cols="30" rows="5" class="form-control" required><?php if ($alamat != false) {
+																										echo $alamat['alamat'];
+																									} ?></textarea>
 					</div>
 					<div>
 						<?php echo anchor('auth/info_akun/', '<button type="button" class="btn btn-danger mr-1" >Batal</button>') ?>
